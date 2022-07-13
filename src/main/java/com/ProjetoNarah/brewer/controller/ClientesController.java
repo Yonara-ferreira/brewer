f0b113f7d598c6f65983c11ef3dfecabab0f626d
Yonara-ferreira <yonara.fd@gmail.com>
2022-07-13 10:24:15 -0300

    novo controller e inicio do layout cadastro cliente

---
+++ src/main/java/com/ProjetoNarah/brewer/controller/ClientesController.java
@@ -0,0 +1,32 @@
+package com.ProjetoNarah.brewer.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.ProjetoNarah.brewer.model.Cliente;
+
+@Controller
+public class ClientesController {
+
+	@RequestMapping(value ="/clientes/novo", method= RequestMethod.GET)
+	public String novo (Cliente cliente) {
+			return "clientes/CadastroCliente";
+	}
+	
+	@RequestMapping(value ="/clientes/novo", method= RequestMethod.POST)
+	public String cadastrar(@Valid Cliente cliente, BindingResult result, RedirectAttributes attributes) {
+		if(result.hasErrors()) {
+			return novo(cliente);
+		}
+		
+		attributes.addFlashAttribute("mensagem","cliente cadastrado com sucesso");
+		System.out.println(">>> nome: " + cliente.getNome());
+		return "redirect:/clientes/novo";
+	}
+	
+}
---
+++ src/main/java/com/ProjetoNarah/brewer/model/Cliente.java
@@ -0,0 +1,53 @@
+package com.ProjetoNarah.brewer.model;
+
+public class Cliente {
+
+	private long codigo;
+	private String nome;
+	private String tipoPessoa;
+	private String cpfOuCnpj;
+	private String telefone;
+	private String email;
+	
+	
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpfOuCnpj() {
+		return cpfOuCnpj;
+	}
+	public void setCpfOuCnpj(String cpfOuCnpj) {
+		this.cpfOuCnpj = cpfOuCnpj;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTipoPessoa() {
+		return tipoPessoa;
+	}
+	public void setTipoPessoa(String tipoPessoa) {
+		this.tipoPessoa = tipoPessoa;
+	}
+	
+	
+	
+	
+}
