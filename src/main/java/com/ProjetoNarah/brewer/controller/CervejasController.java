0da891bc5b9cef4b7ee928dadfdaf33649cb0377
Yonara-ferreira <yonara.fd@gmail.com>
2022-07-17 17:56:32 -0300

    implementação do logging

--- src/main/java/com/ProjetoNarah/brewer/controller/CervejasController.java
+++ src/main/java/com/ProjetoNarah/brewer/controller/CervejasController.java
@@ -2,6 +2,8 @@ package com.ProjetoNarah.brewer.controller;
 
 import javax.validation.Valid;
 
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,6 +15,9 @@ import com.ProjetoNarah.brewer.model.Cerveja;
 
 @Controller
 public class CervejasController {
+	
+	//private static final Logger logger = LoggerFactory.getLogger(CervejasController.class);
+	
 
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.GET)
 	public String novo(Cerveja cerveja) {
--- src/main/java/com/ProjetoNarah/brewer/model/Cerveja.java
+++ src/main/java/com/ProjetoNarah/brewer/model/Cerveja.java
@@ -41,4 +41,7 @@ public class Cerveja {
 		this.nome = nome;
 	}
 
+	
+	
+
 }
