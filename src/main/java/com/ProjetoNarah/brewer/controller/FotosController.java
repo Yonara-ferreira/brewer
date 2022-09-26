756c54db74c23996513d7cf72f3f509b7aa61af9
Yonara-ferreira <yonara.fd@gmail.com>
2022-09-26 20:31:00 -0300

    Inicio da implementação de novas tabelas no sistema [incompleto- erro na migração]

--- src/main/java/com/ProjetoNarah/brewer/controller/FotosController.java
+++ src/main/java/com/ProjetoNarah/brewer/controller/FotosController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.ProjetoNarah.brewer.dto.FotoDTO;
 import com.ProjetoNarah.brewer.storage.FotoStorageRunnable;
 
 @RestController
@@ -15,8 +16,8 @@ import com.ProjetoNarah.brewer.storage.FotoStorageRunnable;
 public class FotosController {
 
 	@PostMapping
-	public DeferredResult<String> upload(@RequestParam("files[]") MultipartFile[] files)  {
-		DeferredResult<String> resultado = new DeferredResult<>();
+	public DeferredResult<FotoDTO> upload(@RequestParam("files[]") MultipartFile[] files)  {
+		DeferredResult<FotoDTO> resultado = new DeferredResult<>();
 		
 		Thread thread = new Thread(new FotoStorageRunnable(files, resultado));
 		thread.start();
--- src/main/java/com/ProjetoNarah/brewer/storage/FotoStorageRunnable.java
+++ src/main/java/com/ProjetoNarah/brewer/storage/FotoStorageRunnable.java
@@ -3,12 +3,14 @@ package com.ProjetoNarah.brewer.storage;
 import org.springframework.web.context.request.async.DeferredResult;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.ProjetoNarah.brewer.dto.FotoDTO;
+
 public class FotoStorageRunnable implements Runnable {
 	
 	private MultipartFile[] files;
-	private DeferredResult<String> resultado;
+	private DeferredResult<FotoDTO> resultado;
 
-	public FotoStorageRunnable(MultipartFile[] files, DeferredResult<String> resultado) {
+	public FotoStorageRunnable(MultipartFile[] files, DeferredResult<FotoDTO> resultado) {
 		this.files = files;
 		this.resultado = resultado;
 	}
@@ -18,7 +20,9 @@ public class FotoStorageRunnable implements Runnable {
 	@Override
 	public void run() {
 		System.out.println(">>> files : " + files[0].getSize());
-		resultado.setResult("ok, deu bom");
+		String nomeFoto = files[0].getOriginalFilename();
+		String contentType = files [0].getContentType();
+		resultado.setResult(new FotoDTO(nomeFoto, contentType));
 
 	}
 }
