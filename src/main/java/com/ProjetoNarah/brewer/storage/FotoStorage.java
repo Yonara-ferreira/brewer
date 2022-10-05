b292536ceb8d8fd2bcdb02d770c9bf403fb3f863
Yonara-ferreira <yonara.fd@gmail.com>
2022-10-04 21:54:39 -0300

    Salvando imagens temporarias e implementando exibiçao das fotos na tela de cadastro de cerveja

--- src/main/java/com/ProjetoNarah/brewer/storage/FotoStorage.java
+++ src/main/java/com/ProjetoNarah/brewer/storage/FotoStorage.java
@@ -5,6 +5,8 @@ import org.springframework.web.multipart.MultipartFile;
 public interface FotoStorage {
 	
 	
-	public void salvarTemporariamente(MultipartFile[] files);
+	public String salvarTemporariamente(MultipartFile[] files);
+
+	public byte[] recuperarFotoTemporaria(String nome);
 
 }
--- src/main/java/com/ProjetoNarah/brewer/storage/local/FotoStorageLocal.java
+++ src/main/java/com/ProjetoNarah/brewer/storage/local/FotoStorageLocal.java
@@ -2,9 +2,13 @@ package com.ProjetoNarah.brewer.storage.local;
 
 import static java.nio.file.FileSystems.getDefault;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.UUID;
+
+
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,21 +20,36 @@ public class FotoStorageLocal implements FotoStorage {
 	
 	private static final Logger logger = LoggerFactory.getLogger(FotoStorageLocal.class);
 	
+	
+	@Override
+	public byte[] recuperarFotoTemporaria(String nome) {
+		try {
+			return Files.readAllBytes(this.localTemporario.resolve(nome));
+		} catch (IOException e) {
+			throw new RuntimeException("Erro na leitura da foto temporaria", e);
+		
+		}
+	}
+	
 	private Path local;
 	private Path localTemporario;
+	
+	
 	
 	
 	public FotoStorageLocal() {
 		this(getDefault().getPath(System.getenv("HOME"), ".brewerfotos"));
 	}
 	
+	
+	
 	public FotoStorageLocal(Path path) {
 		this.local = path;
 		criarPastas();
 	}
 	
-
-
+	
+	
 	private void criarPastas() {
 		try {
 			Files.createDirectories(this.local);
@@ -45,15 +64,32 @@ public class FotoStorageLocal implements FotoStorage {
 		} catch (IOException e) {
 			throw new RuntimeException("Erro na criação da pasta para salvar fotos", e);
 		}
-		
-		
 	}
 
 
+	
 	@Override
-	public void salvarTemporariamente(MultipartFile[] files) {
-		System.out.println(">>>salvando fotos temporarias ... ");
+	public String salvarTemporariamente(MultipartFile[] files) {
+		String novoNome = null;
+		if (files != null && files.length >0 ) {
+			MultipartFile arquivo = files[0];
+			novoNome = renomearArquivo(arquivo.getOriginalFilename());
+			try {
+				arquivo.transferTo(new File(this.localTemporario.toAbsolutePath().toString() + getDefault().getSeparator() + novoNome));
+			} catch ( IOException e) {
+				throw new RuntimeException("erro ao salvar a foto na pasta temporaria", e);
+			}
+		}
+		return novoNome;
+	}
+	
+	private String renomearArquivo(String nomeOriginal) {
+		String novoNome = UUID.randomUUID().toString() +  "__" + nomeOriginal;
 		
+		if(logger.isDebugEnabled()) {
+			logger.debug(String.format("nome original: %s, novo nome: %s",nomeOriginal, novoNome));
+		}
+		return novoNome;
 	}
 
 }
