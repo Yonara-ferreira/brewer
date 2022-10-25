b121f3454bbc6ac477681d8fb4cfb0ca7083d258
Yonara-ferreira <yonara.fd@gmail.com>
2022-10-24 22:32:55 -0300

    Modelando pagina de 'pesquisa de cerveja' (inacabada)

--- src/main/java/com/ProjetoNarah/brewer/controller/CervejasController.java
+++ src/main/java/com/ProjetoNarah/brewer/controller/CervejasController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -52,5 +53,15 @@ public class CervejasController {
 		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
 		return new ModelAndView ("redirect:/cervejas/novo");
 	}
+	
+	@GetMapping
+	public ModelAndView pesquisar() {
+		ModelAndView mv = new ModelAndView("cerveja/PesquisaCervejas");
+		mv.addObject("estilos", estilos.findAll());
+		mv.addObject("sabores", Sabor.values());
+		return mv;
+	}
+	
+	
 	
 }
