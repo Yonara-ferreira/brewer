90875c744e371493f9d181c0a15e3789376b43cf
Yonara-ferreira <yonara.fd@gmail.com>
2023-03-07 16:39:18 -0300

    Aplicando migração de cliente e aprendendo sobre o @embeddable

--- src/main/java/com/ProjetoNarah/brewer/model/Cliente.java
+++ src/main/java/com/ProjetoNarah/brewer/model/Cliente.java
@@ -1,84 +1,123 @@
 package com.ProjetoNarah.brewer.model;
 
-public class Cliente {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long codigo;
+	
 	private String nome;
-	private String tipoPessoa;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name = "tipo_pessoa")
+	private TipoPessoa tipoPessoa;
+	
+	@Column(name = "cpf_cnpj")
 	private String cpfOuCnpj;
+	
 	private String telefone;
-	private String logradouro;
-	private String cidade;
-	private String estado;
-	private String numero;
-	public String getNumero() {
-		return numero;
-	}
-	public void setNumero(String numero) {
-		this.numero = numero;
-	}
+	
 	private String email;
 	
-	
+	@Embedded
+	private Endereco endereco;
+
 	public long getCodigo() {
 		return codigo;
 	}
+
 	public void setCodigo(long codigo) {
 		this.codigo = codigo;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+	public TipoPessoa getTipoPessoa() {
+		return tipoPessoa;
+	}
+
+	public void setTipoPessoa(TipoPessoa tipoPessoa) {
+		this.tipoPessoa = tipoPessoa;
+	}
+
 	public String getCpfOuCnpj() {
 		return cpfOuCnpj;
 	}
+
 	public void setCpfOuCnpj(String cpfOuCnpj) {
 		this.cpfOuCnpj = cpfOuCnpj;
 	}
+
 	public String getTelefone() {
 		return telefone;
 	}
+
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public String getTipoPessoa() {
-		return tipoPessoa;
+
+	public Endereco getEndereco() {
+		return endereco;
 	}
-	public void setTipoPessoa(String tipoPessoa) {
-		this.tipoPessoa = tipoPessoa;
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
 	}
-	public String getLogradouro() {
-		return logradouro;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (codigo ^ (codigo >>> 32));
+		return result;
 	}
-	public void setLogradouro(String logradouro) {
-		this.logradouro = logradouro;
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (codigo != other.codigo)
+			return false;
+		return true;
 	}
-	public String getCidade() {
-		return cidade;
-	}
-	public void setCidade(String cidade) {
-		this.cidade = cidade;
-	}
-	public String getEstado() {
-		return estado;
-	}
-	public void setEstado(String estado) {
-		this.estado = estado;
-	}
+
 	
 	
-	
-	
-	
-	
+
 	
 }
