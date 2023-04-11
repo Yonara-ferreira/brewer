03127ffcd1fc29cb644249ba99dd576d92c8f722
Yonara-ferreira <yonara.fd@gmail.com>
2023-04-10 21:42:31 -0300

    Criado novas tabelas e iniciando coorelações usando JoinCollumns

--- src/main/java/com/ProjetoNarah/brewer/model/Usuario.java
+++ src/main/java/com/ProjetoNarah/brewer/model/Usuario.java
@@ -1,66 +1,143 @@
 package com.ProjetoNarah.brewer.model;
 
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
 
-public class Usuario {
-
-	private String codigo;
+@Entity
+@Table(name="usuario")
+public class Usuario implements Serializable {
 	
-	@NotBlank(message = "Nome é obrigatorio")
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo; 
+	
+	@NotNull(message = "Nome é obrigatório!!!")
 	private String nome;
 	
-	@NotBlank(message = "E-mail é obrigatoria")
+	@NotBlank(message = "Email é obrigatório")
+	@Email(message = "E-mail invalido")
 	private String email;
-	
+
 	@Size(min=5, max=15, message = "Senha nao corresponde com os requisitos" )
 	@NotBlank(message = "Por gentileza,insira uma senha!")
 	private String senha;
 	
-	private String confirmacaoDeSenha;
+	private boolean ativo;
 	
+	@NotNull(message = "Selecione pelo menos um grupo")
+	@ManyToMany
+	@JoinTable(name = "usuario_grupo", joinColumns = @JoinColumn(name = "codigo_usuario")
+										, inverseJoinColumns = @JoinColumn(name = "codigo_grupo"))
+	private List<Grupo> grupos;
 	
-	private int dataDeNasc;
-	
-	
-	
-	public String getCodigo() {
+	@NotNull(message = "Data de nascimento é obrigatório")
+	@Column(name = "data_nascimento")
+	private LocalDate dataNascimento;
+
+	public Long getCodigo() {
 		return codigo;
 	}
-	public void setCodigo(String codigo) {
+
+	public void setCodigo(Long codigo) {
 		this.codigo = codigo;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getSenha() {
 		return senha;
 	}
+
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
-	public int getDataDeNasc() {
-		return dataDeNasc;
+
+	public boolean isAtivo() {
+		return ativo;
 	}
-	public void setDataDeNasc(int dataDeNasc) {
-		this.dataDeNasc = dataDeNasc;
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
 	}
-	public String getConfirmacaoDeSenha() {
-		return confirmacaoDeSenha;
+	
+	
+
+	public List<Grupo> getGrupos() {
+		return grupos;
 	}
-	public void setConfirmacaoDeSenha(String confirmacaoDeSenha) {
-		this.confirmacaoDeSenha = confirmacaoDeSenha;
+
+	public void setGrupos(List<Grupo> grupos) {
+		this.grupos = grupos;
 	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+	
+	
+	
 	
 	
 	
