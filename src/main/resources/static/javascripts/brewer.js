91d53c8126025627f2f08587937c630c1a63d49e
Yonara-ferreira <yonara.fd@gmail.com>
2023-04-17 20:27:48 -0300

    Parte final do input com formatação de Date - Completo

--- src/main/resources/static/javascripts/brewer.js
+++ src/main/resources/static/javascripts/brewer.js
@@ -57,6 +57,20 @@ Brewer.MaskCep = (function() {
 	
 }());
 
+Brewer.MaskDate = (function(){ 
+	
+	function MaskDate(){
+		this.inputDate = $('.js-date');
+	}
+	
+	MaskDate.prototype.enable = function(){
+		this.inputDate.mask('00/00/0000');
+	}
+	
+	return MaskDate;
+	
+}());
+
 /* Função após a página ser carregada */
 
 $(function(){
@@ -68,6 +82,8 @@ $(function(){
 		
 	var maskCep = new Brewer.MaskCep();
 		maskCep.enable();
-	
+		
+	var maskDate = new Brewer.MaskDate();
+		maskDate.enable();
 });
 
