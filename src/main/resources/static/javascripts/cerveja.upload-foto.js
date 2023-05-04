9681727d02624aa57b91e738f740b20241c690fd
Yonara-ferreira <yonara.fd@gmail.com>
2023-05-04 19:11:10 -0300

    19.9 - Config CSRF

--- src/main/resources/static/javascripts/cerveja.upload-foto.js
+++ src/main/resources/static/javascripts/cerveja.upload-foto.js
@@ -22,7 +22,8 @@ Brewer.UploadFoto = (function() {
 			filelimit: 1,
 			allow: '*.(jpg|jpeg|png)',
 			action: this.ContainerFotoCerveja.data('url-fotos'),
-			complete: onUploadCompleto.bind(this)
+			complete: onUploadCompleto.bind(this),
+			beforeSend: adicionarCsrfToken
 		}
 
 		UIkit.uploadSelect($('#upload-select'), settings);
@@ -52,6 +53,12 @@ Brewer.UploadFoto = (function() {
 		this.uploadDrop.removeClass('hidden');
 		this.inputNomeFoto.val('');
 		this.inputContentType('');
+	}
+	
+	function adicionarCsrfToken(xhr){
+		var token = $('input[name=_csrf]').val();
+		var header = $('input[name=_csrf_header]').val();
+		xhr.setRequestHeader(header, token);
 	}
 
 	return UploadFoto;
